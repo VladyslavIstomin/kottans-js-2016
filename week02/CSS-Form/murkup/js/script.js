c3fb25258f277d17643537eb1a90f0e12844b14e
Vladyslav_Istomin <Vladyslav_Istomin@epam.com>
2016-06-03 11:56:45 +0300

    Finish css form

--- week02/CSS-Form/murkup/js/script.js
+++ week02/CSS-Form/murkup/js/script.js
@@ -3,10 +3,6 @@
 
 	$('.select').on('click', function(e) {
 		var selectList = $(this).find('.select-list');
-		if (selectList[0].hidden) {
-			selectList[0].hidden = false;
-		} else {
-			selectList[0].hidden = true;
-		}
+		selectList[0].hidden ? selectList[0].hidden = false : selectList[0].hidden = true
 	})
 })();
